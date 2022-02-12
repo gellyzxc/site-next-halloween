1dfe2785e0dbbd51f5c642bc3fe2f92474f745f8
gellyzxc <urasaftn@gmail.com>
2022-02-12 15:46:40 +0500

    initial markup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 export default function Index() {
   return(
-    <div>
-      Главная страница
+    <div className="site">
+      <div className="site__wrapper">
+      <div className="site__menu">menu</div>
+      <div className="site__content">
+        <div className="content__slider">slaider</div>
+      </div>
+      </div>
     </div>
+
   );
-}
+} 
