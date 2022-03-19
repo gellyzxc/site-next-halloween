d7aa6b315c3475d3b75c189a8b18e1a3da665651
Общий профиль ptpit <ptpit@pl1.edu>
2022-03-19 15:40:53 +0500

    askdask

--- components/slider1.js
+++ components/slider1.js
@@ -1,8 +1,24 @@
+import tikva from "../img/home1-img.png"
+import Image from "next/image";
 export default function Slider1() {
     return(
         <div className="content__slider">
             <div className="content__slider_image">
-                <Image src={tikva} ></Image>
+                <Image src={tikva} width="360" height="400"></Image>
+            </div>
+            <div className="content__slider_main_block">
+                <div className="content__slider_main_text_1">
+
+                </div>
+                <div className="content__slider_main_text_2">
+                    
+                </div>
+                <div className="content__slider_main_text_3">
+                    
+                </div>
+                <div className="content__slider_main_buttons">
+                    
+                </div>
             </div>
         </div>
     )
